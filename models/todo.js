194ffcf291dec9716be0a4109ccb464986503947
raul-octavian <alburaul@gmail.com>
2022-03-07 07:48:08 +0100

    adds versioning for todos

--- models/todo.js
+++ models/todo.js
@@ -24,7 +24,25 @@ let todoSchema = new Schema({
   time: {
     type: Number, min: 0, max: 1440
   }
-  
+
+});
+
+todoSchema.pre('findByIdAndUpdate', function () {
+  const update = this.getUpdate();
+  if (update.__v != null) {
+    delete update.__v;
+  }
+  const keys = ['$set', '$setOnInsert'];
+  for (const key of keys) {
+    if (update[key] != null && update[key].__v != null) {
+      delete update[key].__v;
+      if (Object.keys(update[key]).length === 0) {
+        delete update[key];
+      }
+    }
+  }
+  update.$inc = update.$inc || {};
+  update.$inc.__v = 1;
 });
 
 module.exports = mongoose.model('todo', todoSchema);
